2c64b77e0e5c40c0396e9a2182ec3b0d7b2b2705
Marla Brizel <marlabrizel@gmail.com>
2015-07-11 18:04:47 -0600

    refactor self.create and self.update

--- app/models/robot_repository.rb
+++ app/models/robot_repository.rb
@@ -10,51 +10,35 @@ class RobotRepository
     end
   end
 
-  def self.raw_robots
-    database.transaction do
-      database['robots'] || []
-    end
-  end
-
   def self.all
     dataset.to_a.map { |data| Robot.new(data)}
   end
 
-  def self.raw_robot(id)
-    raw_robots.find { |robot| robot["id"] == id }
-  end
-
   def self.create(robot)
-    dataset.insert(name: robot[:name],
-                                  city: robot[:city],
-                                  state: robot[:state],
-                                  avatar: robot[:avatar],
-                                  birthdate: robot[:birthdate],
-                                  date_hired: robot[:date_hired],
-                                  department: robot[:department])
+    # begin
+      dataset.insert(name: robot[:name],
+                     city: robot[:city],
+                     state: robot[:state],
+                     avatar: robot[:avatar],
+                     birthdate: robot[:birthdate],
+                     date_hired: robot[:date_hired],
+                     department: robot[:department])
+    # rescue
+    #   return false
+    # end
+    # Task.new(dataset.to_a.last)
   end
 
   def self.find(id)
-    Robot.new(raw_robot(id))
+    Robot.new(dataset.where(id: id).first)
   end
 
-  def self.update(id, robot)
-    database.transaction do
-      target = database['robots'].find { |data| data["id"] == id }
-      target["name"] = robot[:name]
-      target["city"] = robot[:city]
-      target["state"] = robot[:state]
-      target["avatar"] = robot[:avatar]
-      target["birthdate"] = robot[:birthdate]
-      target["date_hired"] = robot[:date_hired]
-      target["department"] = robot[:department]
-    end
+  def self.update(id, robot={})
+    target = dataset.where(id: id).update(robot)
   end
 
   def self.delete(id)
-    database.transaction do
-      database['robots'].delete_if { |robot| robot["id"] == id }
-    end
+    dataset.where(id: id).delete
   end
 
   def self.delete_all
